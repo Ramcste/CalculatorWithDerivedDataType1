d5a2127bdbc100274a41c6f51e27b98f7fe8799c
Ramcste <lucky.guyram@gmail.com>
2015-04-29 03:46:05 +0600

    Calculator With Derived Data Type

---
+++ CalculatorWithDerivedDataType/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWithDerivedDataType
+{
+    class Calculator
+    {
+        //public double firstNumber;
+        //public double secondNumber;
+        //public double result;
+
+
+        public double GetAddition(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double GetSubtraction(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public double GetMultiplication(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public double GetDivision(double firstNumber, double secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+       
+    }
+}
---
+++ CalculatorWithDerivedDataType/CalculatorUi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace CalculatorWithDerivedDataType
+{
+    public partial class CalculatorUi : Form
+    {
+        Calculator aNumber =new Calculator();
+
+        public CalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+
+            reultTextBox.Text = aNumber.GetAddition(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text)).ToString();
+        
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+           
+
+            reultTextBox.Text = aNumber.GetSubtraction(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text)).ToString();
+        
+        }
+
+      
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+                                
+            reultTextBox.Text = aNumber.GetMultiplication(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text)).ToString();
+        
+        }
+        
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            
+             reultTextBox.Text = aNumber.GetDivision(double.Parse(firstNumberTextBox.Text), double.Parse(secondNumberTextBox.Text)).ToString();
+
+        }
+
+        
+    }
+}
